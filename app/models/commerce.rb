ef5fccbd8545f704e26b87544d4538b655dc7c11
Santiago Torres G <thdealer@gmail.com>
2020-01-17 18:33:21 -0500

    adding validations

--- app/models/commerce.rb
+++ app/models/commerce.rb
@@ -2,5 +2,6 @@ class Commerce < ApplicationRecord
   belongs_to :user
   belongs_to :country
   belongs_to :commerce_type
+  validates_presence_of :name, :user, :commerce_type, :email, :country, :city, :state, :address
 end
 ext

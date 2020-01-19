ea6e6fccf3a740bf18ea9531b71771df994f6fb0
Santiago Torres G <thdealer@gmail.com>
2020-01-19 03:34:35 -0500

    updating specs for user and commerce model

--- spec/models/commerce_spec.rb
+++ spec/models/commerce_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Commerce, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:country) }
+  it { should belong_to(:commerce_type) }
+  it { should have_many(:photos) }
+  it { should have_many(:reservations) }
+  it { should have_many(:favorites) }
 end

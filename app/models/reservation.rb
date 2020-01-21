e3d48ed1156506fcb1cdcc13022b0a3edc782693
Santiago Torres G <thdealer@gmail.com>
2020-01-21 15:28:18 -0500

    adding validtions and improving serializer

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,5 @@ class Reservation < ApplicationRecord
   belongs_to :commerce
 
   validates_presence_of :commerce, :user, :seat, :date_time
+  validates :user, uniqueness: { scope: :commerce }
 end
--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,4 +1,5 @@
 class ReservationSerializer < ActiveModel::Serializer
-  attributes :id, :seat, :date_time, :user
+  attributes :id, :seat, :date_time
   belongs_to :user, serializer: UserSerializer
+  belongs_to :commerce, serializer: CommerceSerializer
 end

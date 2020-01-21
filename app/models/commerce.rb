6d3d2f32d3f2752340671075dd91151565db8386
Santiago Torres G <thdealer@gmail.com>
2020-01-21 15:04:37 -0500

    adding dependant destroy to children entities

--- app/models/commerce.rb
+++ app/models/commerce.rb
@@ -1,13 +1,12 @@
 class Commerce < ApplicationRecord
   belongs_to :user
-  belongs_to :user
   belongs_to :country
   belongs_to :commerce_type
 
-  has_many :photos, as: :imageable
-  has_many :reservations
-  has_many :favorites
+  has_many :photos, as: :imageable, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates_presence_of :name, :user, :commerce_type, :email, :country, :city
-  validates :email, uniqueness: true
+  validates_uniqueness_of :email, allow_blank: false
 end

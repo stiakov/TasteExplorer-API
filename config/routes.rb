180048a88cc0f1a96518d452788273ed2398e78b
Santiago Torres G <thdealer@gmail.com>
2020-01-21 17:54:57 -0500

    adding photos controller an routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,8 @@ Rails.application.routes.draw do
   end
 
   namespace :v1 do
-    get 'commerces/index'
     post 'commerces/create'
-    get 'commerces/show/:id', to: 'commerces#show'
-    put 'commerces/:id/edit', to: 'commerces#update'
+    get 'commerces/show'
     delete 'commerces/:id/delete', to: 'commerces#destroy'
   end
 
@@ -28,9 +26,24 @@ Rails.application.routes.draw do
   end
 
   namespace :v1 do
+    get 'routes/index'
+    post 'routes/create'
+    get 'routes/show/:id', to: 'routes#show'
+    put 'routes/:id/edit', to: 'routes#update'
+    delete 'routes/:id/delete', to: 'routes#destroy'
+  end
+
+  namespace :v1 do
     get 'favorites/index'
     post 'favorites/create'
     get 'favorites/show/:id', to: 'favorites#show'
     delete 'favorites/:id/delete', to: 'favorites#destroy'
+  end
+
+  namespace :v1 do
+    post 'photos/create'
+    get 'photos/show/user/:id', to: 'photos#show_user'
+    get 'photos/show/commerce/:id', to: 'photos#show_commerce'
+    delete 'photos/destroy'
   end
 end
